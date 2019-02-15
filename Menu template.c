9bb888b3584339b96ad9ae4592fbc3e46b6dd4b7
EliseyBelka <46591130+EliseyBelka@users.noreply.github.com>
2019-02-15 10:59:35 +0700

    Add files via upload

---
+++ Menu template.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <math.h>
+#define CompletedTask 4 
+
+void printMenu();
+void task_1();
+void task_2();
+void task_3();
+void task_4();
+
+int main(int argc, const char * argv[])
+{
+	printMenu();
+}
+
+
+void printMenu() {
+	int answer = 0;
+	do {
+		printf("Completed tasks: %d\nSelect task number:\n", TASKS_DONE);
+		printf("0. Exit\n1. Task1;\n2. Task2;\n3. Task3;\n4. Task4;\n");
+		scanf("%d", &answer);
+	} while (answer < 0 || answer > TASKS_DONE);
+
+	switch (answer) {
+	case 0:
+		return;
+	case 1:
+		task_1();
+		break;
+	case 2:
+		task_2();
+		break;
+	case 3:
+		task_3();
+		break;
+	case 4:
+		task_4();
+		break;
+
+	default:
+		break;
+	}
+	printf("\n");
+	printMenu();
+}
+
+
+void task_1() {
+	printf("Body task_1");
+}
+
+void task_2() {
+	printf("Body task_2");
+ }
+
+
+void task_3() {
+	printf("Body task_3");
+}
+
+void task_4() {
+	printf("Body task_4");
+}
+
+
+
